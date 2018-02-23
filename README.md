e55c7e0007e325e3afcb40b6a5d8cfda3a0ae05d
adwait <adongare@cmu.edu>
2018-02-22 19:16:35 -0500

    added instructions

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # totalstation-tools
 Tools to use and manipulate total station data
+
+# Installation
+
+Requires a setup with python2.7. You can setup a virtualenv or conda env for this.
+
+1. Clone repository
+
+    ```
+    # clone repository from Github
+    git clone https://github.com/WiseLabCMU/totalstation-tools.git
+    # clone totalopenstation as a submodule
+    # (if you do not want to set this up yourself, you should skip the submodule initialization)
+    git submodule update --init
+    ```
+
+1. Install totalopenstation
+
+
+    ```
+    # install dependencies
+    pip install pygeoif
+    pip install pyserial
+    # install the totalopenstation package
+    # make sure the installed version is newer than 0.4.0
+    pip install -v -v -v -e ./totalopenstation
+    ```
+
+# Usage
+
+```
+parseRaw.py <Nikon RAW file>
+```
+
+# Known issues
+
+- Currently empty
+
+*This is free to use software. People nicknamed Patty might need other permissions*
---
+++ parseRaw.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python2.7
+
+import sys
+from totalopenstation.formats import nikon_raw_v200 as nikon
+from totalopenstation.formats import UNKNOWN_STATION
+
+if __name__ == "__main__":
+
+	if len(sys.argv) != 2:
+		print "usage: %s <input file>" % sys.argv[0]
+	else:
+		# open and check existence of file
+		filename = sys.argv[1]
+	try:
+		with open(filename, 'r') as f:
+			filedata = f.read()
+			f.close()
+
+		parsedData = nikon.FormatParser(filedata)
+		pts = parsedData.points
+		# compute and print outputs
+		print "type id: (x,y,z)"
+		for pt in pts:
+			ptid = pt.id
+			pttype = pt.properties['desc']
+			# totalopenstation defaults the theodolite location to UNKNOWN_STATION
+			# currently if it's location doesnt exist in the RAW file this hack
+			# subtracts that location so we get all our locations assuming the
+			# theodolite was at (0,0,0)
+			x = pt.geometry.x - UNKNOWN_STATION.x
+			y = pt.geometry.y - UNKNOWN_STATION.y
+
+			if pttype == "ST":
+				z = pt.geometry.z - UNKNOWN_STATION.z + pt.properties['ih']
+			else:	
+				z = pt.geometry.z - UNKNOWN_STATION.z
+
+			print "%s %s: (%.3f,%.3f,%.3f)" % (pttype, ptid, x, y, z)
+
+	except IOError:
+		print "file %s not found" % filename
